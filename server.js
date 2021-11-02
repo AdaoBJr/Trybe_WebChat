ddfc7fcaffa74364b47dc6e88b4154bf52788d60
Thiago Leite <leite.tjs@gmail.com>
2021-11-02 00:47:50 -0300

    some progress i think

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 const express = require('express');
-const http = require('http');
-
-require('dotenv').config();
-
-const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
 const app = express();
-const server = http.createServer(app);
+const PORT = 3000;
 
-app.get('/', (_req, res) => {
-  res.send('<h1>Welcome!</h1>');
+const socketIoServer = require('http').createServer(app);
+const io = require('socket.io')(socketIoServer, {
+  cors: {
+    origin: `http://localhost${PORT}`,
+    methods: ['GET', 'POST'],
+  },
 });
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+const messages = [{ nickname: 'Thiago Leite', chatMessage: 'teste' }];
+
+io.on('connection', (socket) => {
+  console.log(`Socket conectado com ID: ${socket.id}`);
+  socket.on('message', (data) => {
+    console.log(messages);
+    messages.push(data);
+    io.emit('message', messages);
+  });
 });
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index', { messages });
+});
+
+socketIoServer.listen(PORT, () => console.log(`listening on port ${PORT}`));
