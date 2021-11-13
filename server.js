1e795feeb34ce481f5ab000bd1c7a33bd6b74be9
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-11-13 17:38:31 -0300

    req 1 feito

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
-// Faça seu código aqui
 const express = require('express');
+
 const app = express();
 const http = require('http').createServer(app);
+const moment = require('moment');
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
 const io = require('socket.io')(http, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: `http://localhost:${port}`,
     methods: ['GET', 'POST'],
-  }
+  },
+});
+
+// Fonte moment().format(): https://ifpb.github.io/jaguaribetech/2016/09/01/moment-js/#:~:text=Para%20rodar%20a%20Moment%20no,funcionalidades%20de%20manipula%C3%A7%C3%A3o%20de%20datas.
+io.on('connection', (socket) => {
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const date = moment().format('DD-MM-yyyy HH:mm:ss');
+    io.emit('message', `${date} - ${nickname} - ${chatMessage}`);
+    console.log(`Usuário conectado. ID: ${socket.id}`);
+  });
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(_dirname + '/index.html');
+  res.sendFile(`${__dirname}/views/index.html`);
 });
 
 http.listen(port, () => {
-  console.log(`Server listening on ${port}`)
+  console.log(`Server listening on ${port}`);
 });
