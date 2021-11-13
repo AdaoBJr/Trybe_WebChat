503fc4c711945c4763c56586957688e359d9f4bf
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-11-13 15:55:22 -0300

    cria configuraçao basica do arquivo server

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-// Faça seu código aqu
+// Faça seu código aqui
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(_dirname + '/index.html');
+});
+
+http.listen(port, () => {
+  console.log(`Server listening on ${port}`)
+});
