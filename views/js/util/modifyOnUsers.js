399391512b91b28141fbc04b07fc60ca17924f33
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-11-12 21:21:17 -0300

    Fix: problems Lint ok again

--- views/js/util/modifyOnUsers.js
+++ views/js/util/modifyOnUsers.js
@@ -1,4 +1,5 @@
 module.exports = (onUsers, newNickName, oldNickName, socket) => {
   // look in the array for the old nickname and add the new one
-  onUsers[onUsers.findIndex((obj) => obj.data === oldNickName)] = { data: newNickName, id: socket };
+  const arr = onUsers;
+  arr[arr.findIndex((obj) => obj.data === oldNickName)] = { data: newNickName, id: socket };
 };
