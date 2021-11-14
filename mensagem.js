bb0277d6530febef0e6e74313f6dd99015be275e
CIGR2021 <appencigr@gmail.com>
2021-11-14 14:10:54 -0300

    alteração da mensagem: formato e inclusão do save.

--- mensagem.js
+++ mensagem.js
@@ -1,9 +1,10 @@
 const moment = require('moment'); // Biblioteca para formato de datas/horas
+const { save } = require('./models/webChat');
 
 const create = (chatMessage, nickname) => {
-  const mensagem = `${moment().format(
-    'DD-MM-YYYY HH:mm:ss A',
-  )} - ${nickname}: ${chatMessage}`;
+  const time = moment().format('DD-MM-YYYY HH:mm:ss A');
+  save({ message: chatMessage, nickname, time });
+  const mensagem = `${time} - ${nickname}: ${chatMessage}`;
   return mensagem;
 };
 
