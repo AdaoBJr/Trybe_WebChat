287d73b220374f0d1eb810837749c1183413b90b
Lara Capila <laracapilak@gmail.com>
2021-11-21 18:22:41 -0300

    Implementa função de novo nickname e de criar lista de usuários online

--- public/chat.js
+++ public/chat.js
@@ -5,31 +5,31 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const inputMessage = document.querySelector('.chatMessage').value;
-  const inputNickname = document.querySelector('.nickname').value;
+  const inputRandomNickname = document.querySelector('.randomNickname').innerText;
 
   if (inputMessage.length) {
     const sendMessage = {
-      nickname: inputNickname,
+      nickname: inputRandomNickname,
       chatMessage: inputMessage,
     };
     socket.emit('message', sendMessage);
   }
+  });
 
-  const createMessage = (message) => {
-    const messagesUl = document.querySelector('.messages');
-    const li = document.createElement('li');
-    // https://pt.stackoverflow.com/questions/56766/adicionar-atributo-a-um-elemento
-    li.setAttribute('data-testid', 'message');
-    li.innerText = message;
-    messagesUl.appendChild(li);
-  };
+const createMessage = (message) => {
+  const messagesUl = document.querySelector('.messages');
+  const li = document.createElement('li');
+  // https://pt.stackoverflow.com/questions/56766/adicionar-atributo-a-um-elemento
+  li.setAttribute('data-testid', 'message');
+  li.innerText = message;
+  messagesUl.appendChild(li);
+};
 
-  socket.on('message', (message) => createMessage(message));
-});
+socket.on('message', (message) => createMessage(message));
 
 // https://www.youtube.com/watch?v=Hr5pAAIXjkA&ab_channel=DevPleno
 const randomString = (length) => {
-  let nickname;
+  let nickname = '';
   do {
     nickname += Math.random().toString(36).substr(2);
   } while (nickname.length < length);
@@ -40,3 +40,31 @@ const randomString = (length) => {
 const inputRandomNickname = document.querySelector('.randomNickname');
 const stringNickname = randomString(16);
 inputRandomNickname.innerHTML = stringNickname;
+
+// salva nickname
+const buttonSaveNickname = document.querySelector('.saveNickname');
+buttonSaveNickname.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const newNickname = document.querySelector('.nickname').value;
+  const message = document.querySelector('.message-saveNickname');
+  message.innerHTML = 'Nickname Salvo';
+  inputRandomNickname.innerHTML = newNickname;
+
+  socket.emit('newNickname', newNickname);
+});
+
+// cria lista usuários online
+const onlineUserList = (listOnline) => {
+  const ulList = document.querySelector('.online');
+  console.log(listOnline);
+  ulList.innerHTML = '';
+  listOnline.forEach((user) => {
+    const createList = document.createElement('li');
+    createList.setAttribute('data-testid', 'online-user');
+    createList.innerHTML = user;
+    ulList.appendChild(createList);
+  });
+};
+
+socket.on('onlineUsers', (list) => onlineUserList(list));
