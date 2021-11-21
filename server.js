a9fe0ef92874ca61fb6240401e207a96df0742c0
Abimael <abimaelr.albu@gmail.com>
2021-11-21 17:20:22 -0300

    Req 1 Done

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
 const express = require('express');
-const io = require('socket.io');
 
 const app = express();
+
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: 'http://localhost:3000', // url aceita pelo cors
+        methods: ['GET', 'POST'], // Métodos aceitos pela url
+    },
+});
+
 const port = '3000';
 
-app.listen(port);
+
+
+io.on('connection', (socket) => {
+    socket.on('message', ({ nickname, chatMessage }) => {
+        const date = new Date();
+        const cdate =`${date.toISOString()}`.slice(0,10).split('-').reverse().join('-')
+        const ctime = String(date.toISOString()).slice(11,19)
+
+        const message = `${cdate} ${ctime} - ${nickname}: ${chatMessage}`
+        io.emit('message', message);
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/view/index.html`);
+});
+
+server.listen(port, () => {
+    console.log('Ativo');
+});
