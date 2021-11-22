993da82b06a8bd13cbe01c43d41a97b0136104a7
Bruno Wedley <brunowedley27@gmail.com>
2021-11-22 15:41:42 -0300

    configuraçao de servidor com primeiro requisito passando

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
 // Faça seu código aqui
+require('dotenv').config();
+
+const app = require('express')();
+const http = require('http').createServer(app);
+const cors = require('cors');
+const moment = require('moment');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    method: ['GET', 'POST'],
+  },
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/views/index.ejs`);
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', ({
+    nickname,
+    chatMessage,
+  }) => {
+    const date = moment().format('DD-MM-YYYY HH:mm:ss'); // DD-MM-yyyy HH:mm:ss ${nickname} ${chatMessage}
+    io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
+    // console.log(date);
+  });
+});
+
+http.listen(PORT, () => console.log(`listening on port ${PORT}`));
