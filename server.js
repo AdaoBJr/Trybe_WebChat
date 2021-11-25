ef6943389fe6e480e85388f4523073514f601190
Daniel Alves de Araujo <araujodaniel321@gmail.com>
2021-11-25 18:20:09 -0300

    another late project delivery

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-// Faça seu código aqui
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'views')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+require('./sockets/chat')(io);
+
+server.listen(PORT, () => {
+  console.log(`Servidor ouvindo na porta ${PORT}`);
+});
