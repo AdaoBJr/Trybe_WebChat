d79e5953ec0210201b3e341916291935a4a5626e
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-24 23:12:29 -0300

    update: adiciona mais portas ao io

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: ['http://localhost:3000', 'http://127.0.0.1:5500'],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:3001/'],
     methods: ['GET', 'POST'],
   } });
 
