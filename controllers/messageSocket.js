adf0fa28e7351c8737b648146e8f5540f8878772
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-24 23:56:48 -0300

    fix: corrige data e retorno

--- controllers/messageSocket.js
+++ controllers/messageSocket.js
@@ -1,12 +1,14 @@
+const moment = require('moment');
 const { addMessageToChat } = require('../models/messages');
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    console.log(`Connected user: ${socket.id}`);
-
+    // console.log(`Connected user: ${socket.id}`);
     socket.on('message', async ({ chatMessage, nickname }) => {
-      const message = await addMessageToChat({ chatMessage, nickname });
-      socket.emit('message', message);
+      const date = moment().format('DD-MM-YYYY HH:mm:ss a');
+      const message = `${date} - ${nickname}: ${chatMessage}`;
+      await addMessageToChat(message);
+      io.emit('message', message);
     });
   });
 };
