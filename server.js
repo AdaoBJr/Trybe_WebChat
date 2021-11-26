db94bbf369cdb7a3266a168cc60bcc6f0977b838
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-26 14:38:32 -0300

    Feat: Conexão inicial do socket io

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-// Faça seu código aqui
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado. ID: ${socket.id} `);
+});
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log(`Servidor ouvindo na porta ${PORT}`);
+});
