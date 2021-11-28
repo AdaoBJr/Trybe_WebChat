2651f8759be1c5cb32cf2046403b9890e25abbec
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-28 18:13:23 -0300

    Feat: Função para criar meansagem

--- views/js/chat.js
+++ views/js/chat.js
@@ -5,7 +5,27 @@ window.onbeforeunload = () => {
   socket.disconnect();
 };
 
-const nickNameRandom = Math.random().toString(36).substring(2, 10)
-+ Math.random().toString(36).substring(2, 10);
+// const nickNameRandom = Math.random().toString(36).substring(2, 10)
+// + Math.random().toString(36).substring(2, 10);
 
-console.log(nickNameRandom);
+const form = document.querySelector('form');
+const inputMessage = document.querySelector('#message-box');
+const inputNickname = document.querySelector('#nickname-box');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  socket.emit('message', { chatMessage: inputMessage.value, nickname: inputNickname.value });
+  inputMessage.value = '';
+  return false;
+});
+
+const createMessage = ({ message }) => {
+  const messages = document.querySelector('#messages');
+  const li = document.createElement('li');
+  li.innerText = message;
+  messages.appendChild(li);
+};
+
+socket.on('message', (message) => {
+  createMessage({ message });
+});
