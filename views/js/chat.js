f39ca6462da8d4bb68d8d0eafa9a055740d7d6ca
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-28 18:59:27 -0300

    Feat: Cria nickname aleatorio

--- views/js/chat.js
+++ views/js/chat.js
@@ -5,12 +5,18 @@ window.onbeforeunload = () => {
   socket.disconnect();
 };
 
-// const nickNameRandom = Math.random().toString(36).substring(2, 10)
-// + Math.random().toString(36).substring(2, 10);
+const nickNameRandom = Math.random().toString(36).substring(2, 10)
++ Math.random().toString(36).substring(2, 10);
 
 const form = document.querySelector('form');
 const inputMessage = document.querySelector('#message-box');
 const inputNickname = document.querySelector('#nickname-box');
+const randomNickname = document.querySelector('#randomNickname');
+randomNickname.innerHTML = nickNameRandom;
+
+if (!inputNickname.length) {
+  inputNickname.value = nickNameRandom;
+}
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -22,6 +28,7 @@ form.addEventListener('submit', (event) => {
 const createMessage = ({ message }) => {
   const messages = document.querySelector('#messages');
   const li = document.createElement('li');
+  li.setAttribute('data-testid', 'message');
   li.innerText = message;
   messages.appendChild(li);
 };
