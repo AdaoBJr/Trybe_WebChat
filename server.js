ba28099b4612a286251b658d01f6bb9b68297155
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-11-28 20:59:46 -0300

    :construction: Requesito 1 :construction:

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const moment = require('moment');
 
 const app = express();
 const http = require('http').createServer(app);
@@ -7,6 +9,7 @@ const http = require('http').createServer(app);
 const PORT = 3000;
 
 app.use(cors());
+
 const io = require('socket.io')(http, {
   cors: {
     origin: `http://localhost:${PORT}`,
@@ -14,10 +17,42 @@ const io = require('socket.io')(http, {
   },
 });
 
+const Models = require('./models');
+
+const users = [];
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+const editedMessage = (chatMessage, nickname) => {
+  const timestampMessage = moment().format('DD-MM-yyyy LTS');
+  Models.createMessage({
+    message: chatMessage,
+    nickname,
+    timestamp: timestampMessage,
+  });
+  io.emit('message', `${timestampMessage} - ${nickname}: ${chatMessage}`);
+};
+
 io.on('connection', (socket) => {
-  console.log(`ID: ${socket.id}`);
+  socket.on('newUser', (nickname) => {
+    users.push({
+      userId: socket.id,
+      nickname,
+    });
+    socket.broadcast.emit('newUser', nickname);
+  });
+
+  socket.on('onlineUsers', () => io.emit('onlineUsers', users));
+
+  socket.on('message', ({ chatMessage, nickname }) => {
+    editedMessage(chatMessage, nickname);
+  });
 });
 
 http.listen(PORT, () => {
+  console.clear();
   console.log(`Ouvindo na porta ${PORT}`);
 });
