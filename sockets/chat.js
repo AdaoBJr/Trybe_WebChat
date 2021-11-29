ce6bbb6b3bc21a846aa602c44a19b4ed2ce7dcee
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-29 01:41:43 -0300

    Feat: Sockets conexão dos users registrado no BD

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,17 +1,35 @@
 const date = require('../helpers/formatDate');
 const Model = require('../models/messageModel');
+const { addUser, updateNickname, disconnect } = require('../helpers/updateUsers');
 
-// const userArray = [];
 const newDate = date();
 
-module.exports = (io) => io.on('connection', async (socket) => {
-  const getAllmessages = await Model.getAll();
-  const messages = getAllmessages
-    .map((value) => `${newDate} - ${value.nickName}: ${value.message}`);
-  socket.emit('listMessages', messages);
+const allMessages = (socket, messagesDb) => {
+   const messages = messagesDb
+    .map((message) => `${newDate} - ${message.nickname}: ${message.message}`);
+  socket.emit('messageList', messages);
+  return messages;
+};
 
+module.exports = (io) => io.on('connection', async (socket) => {
+  socket.on('newUser', (nickname) => {
+    const onlineUsers = addUser({ socketId: socket.id, nickname });
+    io.emit('onlineUsers', onlineUsers);
+  });
+  
+  socket.on('updateNickname', (newNickname) => { 
+    updateNickname(socket.id, newNickname, io); 
+  });
+  
+  const messages = await Model.getAll();
+  allMessages(socket, messages);
+  
   socket.on('message', async ({ chatMessage, nickname }) => {
     await Model.createMessage(chatMessage, nickname, newDate);
     io.emit('message', `${newDate} - ${nickname}: ${chatMessage}`);
   });
-}); 
+
+  socket.on('disconnect', () => { 
+    disconnect(io, socket); 
+  });
+});
