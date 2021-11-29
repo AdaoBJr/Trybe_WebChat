54191e53f31cc40b7394915e19147c909524cb6e
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-28 22:39:34 -0300

    Feat: Socket que lista as mensagens vinda do banco

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,11 +1,17 @@
 const date = require('../helpers/formatDate');
+const Model = require('../models/messageModel');
 
-module.exports = (io) => io.on('connection', (socket) => {
-  socket.on('message', ({ chatMessage, nickname }) => {
-    io.emit('message', `${date()} - ${nickname}: ${chatMessage}`);
+// const userArray = [];
+const newDate = date();
 
-    socket.on('disconnect', () => {
-      socket.broadcast.emit('serverMessage', `${socket.id} acabou de se desconectar! :(`);
-    });
+module.exports = (io) => io.on('connection', async (socket) => {
+  const getAllmessages = await Model.getAll();
+  const messages = getAllmessages
+    .map((value) => `${newDate} - ${value.nickName}: ${value.message}`);
+  socket.emit('listMessages', messages);
+
+  socket.on('message', async ({ chatMessage, nickname }) => {
+    await Model.createMessage(chatMessage, nickname, newDate);
+    io.emit('message', `${newDate} - ${nickname}: ${chatMessage}`);
   });
 }); 
