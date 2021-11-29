c99d113779414260b4e4e55fdeb7e906c81e304c
brunobastosfer <brunobastos@edu.unifor.br>
2021-11-29 13:54:17 -0300

    ajustes

---
+++ public/js/frontEnd.js
@@ -0,0 +1,30 @@
+const crypto = require('crypto');
+const model = require('../../models/data');
+
+const generateUserName = () => crypto.randomBytes(8).toString('hex');
+const users = {};
+
+module.exports = (io) => io.on('connection', async (socket) => {
+  const getMessages = await model.getAllMessages();
+  socket.emit('histories', getMessages);
+  users[socket.id] = generateUserName();
+  if (socket.connected) {
+    socket.emit('joined', users);
+  }
+
+  socket.broadcast.emit('newUser', users);
+
+  socket.on('updateNick', ({ nickname, socketId }) => {
+    users[socketId] = nickname;
+    io.emit('changeAllNick', users);
+  });
+
+  socket.on('message', async ({ chatMessage, nickname }) => {
+    io.emit('message', await model.createMessage({ chatMessage, nickname }));
+  });
+
+  socket.on('disconnect', () => {
+    delete users[socket.id];
+    io.emit('userDisconected', socket.id);
+  });
+});
