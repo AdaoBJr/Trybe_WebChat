a60c75171b6639d1662365dd4bf9abf43ea08c07
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-28 22:38:55 -0300

    Feat: Query que cria as mensagens no banco

--- models/messageModel.js
+++ models/messageModel.js
@@ -6,6 +6,16 @@ const getAll = async () => {
   return messages;
 };
 
+const createMessage = async (message, nickName, date) => {
+  const db = await connection();
+  await db.collection('messages').insertOne({
+    nickName,
+    message,
+    date,
+  });
+};
+
 module.exports = {
   getAll,
+  createMessage,
 };
