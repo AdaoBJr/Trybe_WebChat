86a7828e5ad801350115d4f61dde2069a64f3cd4
walterlbf <walter.lbf.f@gmail.com>
2021-11-29 09:41:03 -0300

    server refatorado e getmesages fetch da api feito

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 const http = require('http');
 
 const server = http.createServer(app);
+
 const PORT = process.env.PORT || 3000;
+
 const io = require('socket.io')(server, {
   cors: {
     origin: `http://localhost${3000}`,
@@ -13,43 +15,19 @@ const io = require('socket.io')(server, {
   },
 });
 
+const { getAllMessages } = require('./src/controllers/messagesController');
+
+require('./src/sockets/message')(io);
+require('./src/sockets/onlineUsers')(io);
+
 app.use(express.static(`${__dirname}/src`));
 
-let onlineUsers = [];
-
-io.on('connection', (socket) => {
-  socket.on('message', (message) => {
-    const { chatMessage, nickname } = message;
-    const now = new Date().toLocaleString().replace(/\//g, '-');
-    io.emit('message', `${now} - ${nickname}: ${chatMessage}`);
-  });
-
-  socket.on('userOnline', (nickname) => {
-    onlineUsers.push([nickname, socket.id]);
-
-    io.emit('userOnline', onlineUsers);
-  });
-  socket.on('disconnect', () => {
-    onlineUsers = onlineUsers.filter(([, id]) => id !== socket.id);
-
-    io.emit('userOnline', onlineUsers);
-  });
-});
-
-io.on('connection', (socket) => {
-  socket.on('updateNickname', (nickname) => {
-    onlineUsers = onlineUsers.map((el) => {
-      if (el[1] === socket.id) return [nickname, socket.id];
-      return el;
-    });
-
-    io.emit('userOnline', onlineUsers);
-  });
-});
+app.get('/messages', getAllMessages);
 
 app.get('/', (_req, res) => {
   res.sendFile(`${__dirname}/src/views/index.html`);
 });
+
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
